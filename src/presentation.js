b4150878874a06d2fe53c13ce99259cdde15736e
Saurabh M. Machave <unknown7bolt@gmail.com>
2019-09-27 23:00:46 +0530

    add heading and title

--- src/presentation.js
+++ src/presentation.js
@@ -42,11 +42,11 @@ export default class Presentation extends React.Component {
         theme={theme}
       >
         <Slide transition={['zoom']} bgColor="primary">
-          <Heading size={1} fit caps lineHeight={1} textColor="secondary">
-            Spectacle Boilerplate
+          <Heading size={1} fit caps lineHeight={3} textColor="secondary">
+            <strike>Higher order Polymorphism</strike>
           </Heading>
           <Text margin="10px 0 0" textColor="tertiary" size={1} fit bold>
-            open the presentation/index.js file to get started
+            Algebra
           </Text>
         </Slide>
         <Slide transition={['fade']} bgColor="tertiary">
